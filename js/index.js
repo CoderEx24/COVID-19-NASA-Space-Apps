4b2cba1fc03c96c48f6cfca01b924e11f4c16735
CoderEx24 <kareemhacker123321@gmail.com>
2020-05-31 23:00:19 +0200

    modified the index.js

--- js/index.js
+++ js/index.js
@@ -6,10 +6,6 @@ var req = new XMLHttpRequest()
 req.onreadystatechange = function() {
 	var canv = document.getElementById('chart')
 	var ctx = canv.getContext('2d')
-	var canvWidth = canv.width
-	var canvHieght = canv.height
-	ctx.fillStyle = "#f0f0f0"
-	ctx.fillRect(0, 0, canvWidth, canvHieght)
 	
 	if (this.readystatus === 4 && this.status !== 200) {
 		ctx.fillText("Oops!\nCouldn't get the data :\'<", 10, 50)
@@ -40,7 +36,7 @@ req.onreadystatechange = function() {
 				data : data.Countries.map((country) => country.TotalDeaths),//put here the data
 				backgroundColor : [ 'rgba(99,255,132,0.2)' ],
 				borderColor : [ 'rgba(99, 255, 132, 1)' ],
-				borderWidth : 10
+				borderWidth : 1
 			} ]
 		},
 		options : {
@@ -58,10 +54,3 @@ req.onreadystatechange = function() {
 
 req.open("GET", APIurl, true);
 req.send();
-
-let current_img = 'air';
-document.getElementById('nav').onmouseover = function(evt) {
-	document.getElementById('img-' + current_img).className = 'hidden';
-	document.getElementById('img-' + evt.target.id).className = '';
-	current_img = evt.target.id;
-};
